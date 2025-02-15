64d6dd9d10d908efd9e6972e1caefab670fbdf42
AnilaDesha <anila.desha094@gmail.com>
2025-02-15 20:50:06 +0530

    New Updata

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,46 +1,41 @@
 import styles from './ContactStyles.module.css';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import instagramDark from '../../assets/instagram-dark.svg'
+import instagramLight from '../../assets/instagram-light.svg'
+import { useTheme } from '../../common/ThemeContext';
 
 function Contact() {
-  return (
-    <section id="contact" className={styles.container}>
-      <h1 className="sectionTitle">Contact</h1>
-      <form action="">
-        <div className="formGroup">
-          <label htmlFor="name" hidden>
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            required
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="email" hidden>
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="message" hidden>
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            required></textarea>
-        </div>
-        <input className="hover btn" type="submit" value="Submit" />
-      </form>
+  const { theme } = useTheme();
+
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const instagramIcon = theme === 'light' ? instagramLight : instagramDark;
+
+return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.info}>
+        <h1>Connect At:</h1>
+        <span>
+          <a href="https://x.com/deshaanila?s=11&t=pl-K8pWp12ZwfDGyg8ji2A" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/anilagopikumar" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/anila-gopikumar" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+          <a href="https://www.instagram.com/anila_gopikumar" target="_blank">
+            <img src={instagramIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+      </div>
     </section>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,14 +2,6 @@ import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/Anila.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterLight from '../../assets/twitter-light.svg';
-import twitterDark from '../../assets/twitter-dark.svg';
-import githubLight from '../../assets/github-light.svg';
-import githubDark from '../../assets/github-dark.svg';
-import linkedinLight from '../../assets/linkedin-light.svg';
-import linkedinDark from '../../assets/linkedin-dark.svg';
-import instagramDark from '../../assets/instagram-dark.svg'
-import instagramLight from '../../assets/instagram-light.svg'
 import CV from '../../assets/CV.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
@@ -17,12 +9,6 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
-  const githubIcon = theme === 'light' ? githubLight : githubDark;
-  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
-  const instagramIcon = theme === 'light' ? instagramLight : instagramDark;
-
-
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
@@ -43,23 +29,9 @@ function Hero() {
           Anila Desha
         </h1>
         <h2>Software Developer</h2>
-        <span>
-          <a href="https://x.com/deshaanila?s=11&t=pl-K8pWp12ZwfDGyg8ji2A" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
-          </a>
-          <a href="https://github.com/anilagopikumar" target="_blank">
-            <img src={githubIcon} alt="Github icon" />
-          </a>
-          <a href="https://www.linkedin.com/in/anila-gopikumar" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin icon" />
-          </a>
-          <a href="https://www.instagram.com/anila_gopikumar" target="_blank">
-            <img src={instagramIcon} alt="Linkedin icon" />
-          </a>
-        </span>
         <a href={CV} download>
           <button className="hover">Resume</button>
-        </a>
+        </a> 
       </div>
     </section>
   );
