a14e20b7407ca5ea5b19cd53e4b54cf537261c73
AnilaDesha <anila.desha094@gmail.com>
2025-04-06 18:35:47 +0530

    new update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,6 +34,9 @@ return (
           <a href="https://www.instagram.com/anila_gopikumar" target="_blank">
             <img src={instagramIcon} alt="Linkedin icon" />
         </a>
+      <p>
+        &copy; 2024 Anila Desha. All rights reserved.
+      </p> 
       </span>
   </div>
   </section>
