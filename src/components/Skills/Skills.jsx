75addbc72ec837992353f07e737d436c7bb512ef
AnilaDesha <anila.desha094@gmail.com>
2025-04-06 19:21:21 +0530

    Updated Skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -22,16 +22,14 @@ function Skills() {
     <br/>  
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="HTML/CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="Angular" />
         <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="Vue" />
         <SkillList src={checkMarkIcon} skill="Node.js" />
         <SkillList src={checkMarkIcon} skill="Bootstrap" />
-        <SkillList src={checkMarkIcon} skill="Tailwind.css" />
-        <SkillList src={checkMarkIcon} skill="SASS/SCSS" />
+        <SkillList src={checkMarkIcon} skill="Tailwind.css/SASS/SCSS" />
       </div>
       <hr />
       <div className={styles.skillList}>
@@ -40,16 +38,13 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="R Programming" />
         <SkillList src={checkMarkIcon} skill="Data Structures" />
         <SkillList src={checkMarkIcon} skill="Algorithms" />
-        <SkillList src={checkMarkIcon} skill="Data Science" />
-        <SkillList src={checkMarkIcon} skill="Artificial Intelligence" />
-        <SkillList src={checkMarkIcon} skill="Machine Learning" />
-        <SkillList src={checkMarkIcon} skill="Deep Learning" />
+        <SkillList src={checkMarkIcon} skill="Data Science/AI" />
+        <SkillList src={checkMarkIcon} skill="Machine Learning/Deep Learning" />
         <SkillList src={checkMarkIcon} skill="NLP/Reinforcemnt Learning" />
     </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="MySQL" />
-        <SkillList src={checkMarkIcon} skill="MongoDB" />
+        <SkillList src={checkMarkIcon} skill="MySQL/MongoDB" />
     </div>
     <hr/>      
     <div className={styles.skillList}>
@@ -57,8 +52,7 @@ function Skills() {
     </div>      
     <hr/>      
     <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="GCP" />
-        <SkillList src={checkMarkIcon} skill="Azure" />
+        <SkillList src={checkMarkIcon} skill="GCP/Azure" />
     </div>
     <hr/>      
     <div className={styles.skillList}>
@@ -81,15 +75,13 @@ function Skills() {
       
     <h1 className="sectionTitle">Interesting Activities</h1>
     <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Badminton" />
-        <SkillList src={checkMarkIcon} skill="Trekking" />
-        <SkillList src={checkMarkIcon} skill="Scuba Diving" /> 
+        <SkillList src={checkMarkIcon} skill="Playing Badminton" />
+        <SkillList src={checkMarkIcon} skill="Travelling" /> 
         <SkillList src={checkMarkIcon} skill="Painting" /> 
         <SkillList src={checkMarkIcon} skill="Yoga" /> 
         <SkillList src={checkMarkIcon} skill="Cooking" /> 
-        <SkillList src={checkMarkIcon} skill="Travelling" /> 
-        <SkillList src={checkMarkIcon} skill="Reading Books" />
         <SkillList src={checkMarkIcon} skill="Coding" /> 
+        <SkillList src={checkMarkIcon} skill="Reading Books/Adventurous Activities" />
     </div>
     </section>
   );
